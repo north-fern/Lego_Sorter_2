9c6e4f40fcb47e56aea06cbfe6561af3bb7e1095
north-fern <60278154+north-fern@users.noreply.github.com>
2020-03-09 22:49:11 -0400

    ev3 code

---
+++ legoSort2/main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+import ubinascii, urequests, ujson, utime
+
+
+
+# Write your program here
+ev3 = EV3Brick()
+ev3.speaker.beep()
+
+
+
+def SL_setup():
+     urlBase = "https://api.systemlinkcloud.com/nitag/v2/tags/"
+     headers = {"Accept":"application/json","x-ni-api-key":Key}
+     return urlBase, headers
+     
+def Put_SL(Tag, Type, Value):
+     urlBase, headers = SL_setup()
+     urlValue = urlBase + Tag + "/values/current"
+     propValue = {"value":{"type":Type,"value":Value}}
+     try:
+          reply = urequests.put(urlValue,headers=headers,json=propValue).text
+     except Exception as e:
+          print(e)         
+          reply = 'failed'
+     return reply
+
+def Get_SL(Tag):
+     urlBase, headers = SL_setup()
+     urlValue = urlBase + Tag + "/values/current"
+     try:
+          value = urequests.get(urlValue,headers=headers).text
+          data = ujson.loads(value)
+          #print(data)
+          result = data.get("value").get("value")
+     except Exception as e:
+          print(e)
+          result = 'failed'
+     return result
+     
+def Create_SL(Tag, Type):
+     urlBase, headers = SL_setup()
+     urlTag = urlBase + Tag
+     propName={"type":Type,"path":Tag}
+     try:
+          urequests.put(urlTag,headers=headers,json=propName).text
+     except Exception as e:
+          print(e) 
+
+def TurnMotor(pos):
+    if pos == 0:
+        motor.
